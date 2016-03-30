8b7e5ae8021154bf6d6c3ce3792bb1359d4f819c
ronaldo jose da silva <ronaldorjsilva@hotmail.com>
2016-03-30 19:53:15 -0300

    criação de template

--- gamestore/src/main/java/br/com/gamestore/model/Acessorio.java
+++ gamestore/src/main/java/br/com/gamestore/model/Acessorio.java
@@ -17,15 +17,16 @@ import javax.persistence.Table;
  * @author rjs
  */
 @Entity
-@Table(name="tbl_Usuario")
-public class Usuario implements Serializable {
+@Table(name="tbl_Acessorio")
+public class Acessorio implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
-    private String login;
-    private String senha;
+    private double preco;
+    private boolean status;
+    private int quantidade;
 
     public Long getId() {
         return id;
@@ -45,10 +46,10 @@ public class Usuario implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Usuario)) {
+        if (!(object instanceof Acessorio)) {
             return false;
         }
-        Usuario other = (Usuario) object;
+        Acessorio other = (Acessorio) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
@@ -57,7 +58,7 @@ public class Usuario implements Serializable {
 
     @Override
     public String toString() {
-        return "br.com.gamestore.model.user[ id=" + id + " ]";
+        return "br.com.gamestore.model.Acessorio[ id=" + id + " ]";
     }
 
     /**
@@ -75,31 +76,45 @@ public class Usuario implements Serializable {
     }
 
     /**
-     * @return the login
+     * @return the preco
      */
-    public String getLogin() {
-        return login;
+    public double getPreco() {
+        return preco;
     }
 
     /**
-     * @param login the login to set
+     * @param preco the preco to set
      */
-    public void setLogin(String login) {
-        this.login = login;
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 
     /**
-     * @return the senha
+     * @return the status
      */
-    public String getSenha() {
-        return senha;
+    public boolean isStatus() {
+        return status;
     }
 
     /**
-     * @param senha the senha to set
+     * @param status the status to set
      */
-    public void setSenha(String senha) {
-        this.senha = senha;
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
     
 }
