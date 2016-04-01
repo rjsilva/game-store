56e9c425e573f02f66fa54de8d57c7d3ea2891f7
Administrador <Administrador@ENQ>
2016-04-01 20:05:56 -0300

    Novo Template

--- gamestore/src/main/java/br/com/gamestore/controller/AcessoriosBean.java
+++ gamestore/src/main/java/br/com/gamestore/controller/AcessoriosBean.java
@@ -14,28 +14,37 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @SessionScoped
 public class AcessoriosBean {
-
+    
     private Acessorio acessorio;
-
+    
     public String preparaNovoAcessorio() { // Método responsável por inicialziar o Objeto Acessório, para que possamos setar os atributos.
         acessorio = new Acessorio();
         return "cadastroAcessorio";
     }
-
+    
     public void adicionaAcessorio() {
         AcessorioDao dao = new AcessorioDao();
         dao.cadastrar(acessorio);
         FacesMessage msg = new FacesMessage("Registro salvo com sucesso!");
         FacesContext.getCurrentInstance().addMessage(null, msg);
+        limpar();
+    }
+    
+    public void limpar() { // Método responsavel por limpar o Objeto.
+        acessorio.getId();
+        acessorio.setMarca("");
+        acessorio.setNome("");
+        acessorio.setQuantidade(0);
+        acessorio.setStatus(0);
     }
 
     // Getters and Setters
     public Acessorio getAcessorio() {
         return acessorio;
     }
-
+    
     public void setAcessorio(Acessorio acessorio) {
         this.acessorio = acessorio;
     }
-
+    
 }
